eeaaf3fc5f2373f20b9cf72e647b50672bb29a14
Aquatec Innovative <55432822+aquatec9256@users.noreply.github.com>
2020-04-08 19:19:33 +0530

    Polaries css test in index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { Layout, Page, TextStyle } from '@shopify/polaris';
 const Index = () => (
-    <div>
-      <p>Sample app using React and Next.js</p>
-    </div>
+  <Page>
+    <Layout>
+    <TextStyle variation="positive">
+      Sample app using React and Next.js
+    </TextStyle>
+    </Layout>
+  </Page>
   );
   
   export default Index;
