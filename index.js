e54a1739b67e9120c1a60be490fec66e7eacba4b
Aquatec Innovative <55432822+aquatec9256@users.noreply.github.com>
2020-04-09 09:42:16 +0530

    test1

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const next = require('next');
 const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
-
+var router = require('koa-router');
 dotenv.config();
+const _ = router();              //Instantiate the router
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -14,10 +15,19 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
-app.prepare().then(() => {
+//app.prepare().then(() => {
     const server = new Koa();
   server.use(session({ secure: true, sameSite: 'none' }, server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
+  var _ = router();              //Instantiate the router
+_.get('/', getMessage);   // Define routes
+
+function *getMessage() {
+   this.body = "Hello world!";
+};
+
+server.use(_.routes());           //Use the routes defined using the router
+//app.listen(3000)
   server.use(
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
@@ -28,16 +38,16 @@ app.prepare().then(() => {
 
         ctx.redirect('/');
       },
-    }),
+    })
   );
   server.use(verifyRequest());
-  server.use(async (ctx) => {
-    await handle(ctx.req, ctx.res);
-    ctx.respond = false;
-    ctx.res.statusCode = 200;
-    return
-  });
+  // server.use(async (ctx) => {
+  //   await handle(ctx.req, ctx.res);
+  //   ctx.respond = false;
+  //   ctx.res.body = "dfdsfs";
+  //   return
+  // });
   server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`);
   });
-});
+//});
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import { Layout, Page, TextStyle } from '@shopify/polaris';
+import { EmptyState, Layout, Page } from '@shopify/polaris';
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 const Index = () => (
   <Page>
     <Layout>
