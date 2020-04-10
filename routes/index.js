45360d7bb73eab04106b903c94a051e6bf9429e4
Aquatec Innovative <55432822+aquatec9256@users.noreply.github.com>
2020-04-10 13:56:28 +0530

    get ac

--- routes/index.js
+++ routes/index.js
@@ -4,32 +4,48 @@ const auth_controller = require('../controllers/auth.controller');
 const passport = require('passport');
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 var shopifyAPI = require('shopify-node-api');
+var nonce = require('nonce')();
+
 const {
   SHOPIFY_API_SECRET_KEY,
   SHOPIFY_API_KEY,
   HOST,
 } = process.env;
 
-const Shopify = new shopifyAPI({
-  shop: 'techardik.myshopify.com/', // MYSHOP.myshopify.com
-  shopify_api_key: SHOPIFY_API_KEY, // Your API key
-  shopify_shared_secret: SHOPIFY_API_SECRET_KEY, // Your Shared Secret
-  shopify_scope: 'write_products',
-  redirect_uri: HOST+'/finish_auth',
-  nonce: '' // you must provide a randomly selected value unique for each authorization request
-});
+
 router.get('/',forwardAuthenticated,function(req,res,next){
-  var auth_url = Shopify.buildAuthURL();
-console.log(auth_url);
-res.redirect(auth_url);
+  const shop = req.query.shop;
+      if (shop) {
+        console.log(shop);
+      var state=nonce();
+      var Shopify = new shopifyAPI({
+        shop: shop, // MYSHOP.myshopify.com
+        shopify_api_key: SHOPIFY_API_KEY, // Your API key
+        shopify_shared_secret: SHOPIFY_API_SECRET_KEY, // Your Shared Secret
+        shopify_scope: 'write_products',
+        redirect_uri: HOST+'/finish_auth',
+        nonce:state , // you must provide a randomly selected value unique for each authorization request
+      });
+      res.cookie('state', state);
+    var auth_url = Shopify.buildAuthURL();
+    console.log(auth_url);
+    res.redirect(auth_url);
+    }else{
+      return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+    }
 });
 router.get('/finish_auth',forwardAuthenticated,function(req,res,next){
-  var Shopify = new shopifyAPI({ shop: 'techardik.myshopify.com/', // MYSHOP.myshopify.com
+  const { shop, hmac, code, state } = req.query;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
+  if (state !== stateCookie) {
+    return res.status(403).send('Request origin cannot be verified');
+  }
+  var Shopify = new shopifyAPI({ shop: shop, // MYSHOP.myshopify.com
   shopify_api_key: SHOPIFY_API_KEY, // Your API key
   shopify_shared_secret: SHOPIFY_API_SECRET_KEY, // Your Shared Secret
   shopify_scope: 'write_products',
   redirect_uri: HOST+'/finish_auth',
-  nonce: ''}), // You need to pass in your config here
+  nonce: state}), // You need to pass in your config here
     query_params = req.query;
     console.log(query_params);
   Shopify.exchange_temporary_token(query_params, function(err, data){
