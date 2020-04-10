f8c5e4bc53c467fc1eeb94734c4c00943165af49
Aquatec Innovative <55432822+aquatec9256@users.noreply.github.com>
2020-04-10 12:17:09 +0530

    get access

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ const {
   HOST,
 } = process.env;
 
-var Shopify = new shopifyAPI({
-  shop: 'MYSHOP', // MYSHOP.myshopify.com
+const Shopify = new shopifyAPI({
+  shop: 'techardik.myshopify.com/', // MYSHOP.myshopify.com
   shopify_api_key: SHOPIFY_API_KEY, // Your API key
   shopify_shared_secret: SHOPIFY_API_SECRET_KEY, // Your Shared Secret
   shopify_scope: 'write_products',
@@ -20,14 +20,23 @@ var Shopify = new shopifyAPI({
 });
 router.get('/',forwardAuthenticated,function(req,res,next){
   var auth_url = Shopify.buildAuthURL();
-
+console.log(auth_url);
 res.redirect(auth_url);
- 
 });
 router.get('/finish_auth',forwardAuthenticated,function(req,res,next){
-  console.log(req);
+  var Shopify = new shopifyAPI(config), // You need to pass in your config here
+    query_params = req.query;
+    console.log(query_params);
+  Shopify.exchange_temporary_token(query_params, function(err, data){
+    // This will return successful if the request was authentic from Shopify
+    // Otherwise err will be non-null.
+    // The module will automatically update your config with the new access token
+    // It is also available here as data['access_token']
+    console.log(data);
+  });
+
   console.log('res');
-  console.log(res);
+
 
 
   res.render('pages/public/home', {
